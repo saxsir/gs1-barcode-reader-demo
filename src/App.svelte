047f8784a2d29ae8d1e3a0bc3bd91168d8c1a127
saxsir <1664554+saxsir@users.noreply.github.com>
2022-05-17 11:03:41 +0900

    refactor: ロゴいらない

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
   import ZXingReader from './lib/ZXingReader.svelte'
 </script>
 
 <main class="container">
-  <img src={logo} alt="Svelte Logo" />
   <h1>バーコード読み取りデモ</h1>
 
   <ZXingReader />
